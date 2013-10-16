8ce113b9772d38a70332dbe64c9ae0f635fbb4c2
Devaev Maxim <mdevaev@gmail.com>
2013-10-16 14:04:14 +0400

    tools.url.ServerError(): added info()

--- ulib/tools/url.py
+++ ulib/tools/url.py
@@ -18,6 +18,9 @@ class ServerError(Exception) :
     def message(self) :
         return self.__message
 
+    def info(self) :
+        return "%d %s" % (self.__code, self.__message)
+
     def text(self) :
         return self.__text
 
