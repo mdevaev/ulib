293fccedeaf8a20abe8f1c223e35d5c88758afc3
Devaev Maxim <mdevaev@gmail.com>
2013-10-16 14:14:52 +0400

    Added test_url

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ import unittest
 # pylint: disable=W0401,W0614
 from ulib.tests.test_types import *
 from ulib.tests.test_unix import *
+from ulib.tests.test_url import *
 from ulib.tests.test_validators import *
 
 
