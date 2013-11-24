2f88f747a40aaa54effc176460de7d4491747637
Devaev Maxim <mdevaev@gmail.com>
2013-11-24 23:55:20 +0400

    test_system_unix: posix only

--- ulib/tests/test_system_unix.py
+++ ulib/tests/test_system_unix.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 
 from ulib import system
 import ulib.system.unix # pylint: disable=W0611
@@ -6,14 +7,17 @@ import ulib.system.unix # pylint: disable=W0611
 
 ##### Public classes #####
 class TestUnix(unittest.TestCase) :
+    @unittest.skipIf(os.name != "posix", "posix only")
     def test_disk_free(self) :
         (full, used) = system.unix.diskFree("/")
         self.assertTrue(full > used)
 
+    @unittest.skipIf(os.name != "posix", "posix only")
     def test_uptime(self) :
         uptime = system.unix.uptime()
         self.assertTrue(uptime > 0)
 
+    @unittest.skipIf(os.name != "posix", "posix only")
     def test_load_average(self) :
         avg_tuple = system.unix.loadAverage()
         for avg in avg_tuple :
