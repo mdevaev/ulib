1d37cef40c7075ad1523091158307e0f5283fdad
Devaev Maxim <mdevaev@gmail.com>
2013-11-29 14:16:01 +0400

    test_system_unix: skipIf on class

--- ulib/tests/test_system_unix.py
+++ ulib/tests/test_system_unix.py
@@ -6,18 +6,16 @@ import ulib.system.unix # pylint: disable=W0611
 
 
 ##### Public classes #####
+@unittest.skipIf(os.name != "posix", "posix only")
 class TestUnix(unittest.TestCase) :
-    @unittest.skipIf(os.name != "posix", "posix only")
     def test_disk_free(self) :
         (full, used) = system.unix.diskFree("/")
         self.assertTrue(full > used)
 
-    @unittest.skipIf(os.name != "posix", "posix only")
     def test_uptime(self) :
         uptime = system.unix.uptime()
         self.assertTrue(uptime > 0)
 
-    @unittest.skipIf(os.name != "posix", "posix only")
     def test_load_average(self) :
         avg_tuple = system.unix.loadAverage()
         for avg in avg_tuple :
