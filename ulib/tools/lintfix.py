c503f6acae602ba3f6cd6a05b2901667afd393af
Devaev Maxim <mdevaev@gmail.com>
2013-12-14 03:08:21 +0400

    Pylint hacks for pep8 hacks. Inception.

---
+++ ulib/tools/lintfix.py
@@ -0,0 +1,38 @@
+import re
+
+import astroid
+import astroid.nodes
+
+from .tools import pep8
+
+
+##### Public methods #####
+def register(linter):
+    astroid.MANAGER.register_transform(astroid.nodes.Module, __transformPep8)
+    astroid.MANAGER.register_transform(astroid.nodes.Class, __transformPep8)
+
+
+##### Private methods #####
+def __transformPep8(obj) :
+    rename_regexp = re.compile(pep8.RENAME_REGEXP)
+    if isinstance(obj, astroid.nodes.Module) :
+        module_name = obj.name
+    elif isinstance(obj, astroid.nodes.Class) :
+        if not isinstance(obj.parent, astroid.nodes.Module) :
+            return
+        module_name = obj.parent.name
+    else :
+        raise RuntimeError("Invalid input object: %s" % (obj))
+
+    if module_name == "ulib" or module_name.startswith("ulib.") :
+        for name in tuple(obj.locals) :
+            method = obj.locals[name][0]
+            if not isinstance(method, astroid.nodes.Function) or name.startswith("_") :
+                continue
+            pep8_name = re.sub(rename_regexp, pep8.RENAME_PLACEHOLDERS, name).lower()
+            if name != pep8_name :
+                if pep8_name in obj.locals :
+                    raise RuntimeError("Cannot create PEP8 alias: %s.%s -> %s.%s (destination is already exists)" % (
+                        obj.name, name, obj.name, pep8.name ))
+                obj.locals[pep8_name] = obj.locals[name]
+
--- ulib/tools/pep8.py
+++ ulib/tools/pep8.py
@@ -2,13 +2,18 @@ import inspect
 import re
 
 
+##### Public constants #####
+RENAME_REGEXP = r"([a-z0-9])([A-Z])"
+RENAME_PLACEHOLDERS = r"\1_\2"
+
+
 ##### Public methods #####
 def setupAliases(obj = None) :
     if obj is None :
         frame = inspect.stack()[1]
         obj = inspect.getmodule(frame[0])
 
-    rename_regexp = re.compile(r"([a-z0-9])([A-Z])")
+    rename_regexp = re.compile(RENAME_REGEXP)
 
     for name in dir(obj) :
         if name.startswith("_") :
@@ -19,7 +24,7 @@ def setupAliases(obj = None) :
             setupAliases(attr)
 
         elif inspect.isfunction(attr) :
-            pep8_name = re.sub(rename_regexp, r"\1_\2", name).lower()
+            pep8_name = re.sub(rename_regexp, RENAME_PLACEHOLDERS, name).lower()
             if name != pep8_name :
                 if not getattr(obj, pep8_name, None) is None :
                     raise RuntimeError("Cannot create PEP8 alias: %s.%s -> %s.%s (destination is already exists)" % (
