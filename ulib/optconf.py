5f42c89699544c3a7c78642c57bfeb6933351afd
Devaev Maxim <mdevaev@gmail.com>
2013-12-14 03:12:42 +0400

    optconf: pylint fixes

--- ulib/optconf.py
+++ ulib/optconf.py
@@ -8,7 +8,7 @@ from .validatorlib import ValidatorError
 
 
 ##### Public methods #####
-class Namespace :
+class Namespace(argparse.Namespace) : # pylint: disable=R0924,R0903
     def __getitem__(self, option) :
         return getattr(self, option[1])
 
