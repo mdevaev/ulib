afbca5c54960bf282e67999c2a9dc54cb97e4622
Devaev Maxim <mdevaev@gmail.com>
2013-12-16 00:19:59 +0400

    Refactoring

--- ulib/network/url.py
+++ ulib/network/url.py
@@ -37,6 +37,17 @@ class ServerError(Exception) :
 
 
 ##### Public classes #####
+class SocksHandler(urllib.request.HTTPHandler) :
+    def __init__(self, *args_tuple, **kwargs_dict) :
+        urllib.request.HTTPHandler.__init__(self, debuglevel=kwargs_dict.pop("debuglevel", 0))
+        self._args_tuple = args_tuple
+        self._kwargs_dict = kwargs_dict
+
+    def http_open(self, request) :
+        def build(host, port = None, strict = None, timeout = socket._GLOBAL_DEFAULT_TIMEOUT) : # pylint: disable=W0212
+            return SocksConnection(*self._args_tuple, host=host, port=port, strict=strict, timeout=timeout, **self._kwargs_dict)
+        return self.do_open(build, request)
+
 class SocksConnection(http.client.HTTPConnection) :
     def __init__(self, proxy_url = None, proxy_type = None, proxy_host = None, proxy_port = None,
         proxy_user = None, proxy_passwd = None, rdns_flag = True, *args_tuple, **kwargs_dict) :
@@ -66,17 +77,6 @@ class SocksConnection(http.client.HTTPConnection) :
         if not self.timeout is socket._GLOBAL_DEFAULT_TIMEOUT : # pylint: disable=W0212
             self.sock.settimeout(self.timeout)
         self.sock.connect((self.host, self.port))
-
-class SocksHandler(urllib.request.HTTPHandler) :
-    def __init__(self, *args_tuple, **kwargs_dict) :
-        urllib.request.HTTPHandler.__init__(self, debuglevel=kwargs_dict.pop("debuglevel", 0))
-        self._args_tuple = args_tuple
-        self._kwargs_dict = kwargs_dict
-
-    def http_open(self, request) :
-        def build(host, port = None, strict = None, timeout = socket._GLOBAL_DEFAULT_TIMEOUT) : # pylint: disable=W0212
-            return SocksConnection(*self._args_tuple, host=host, port=port, strict=strict, timeout=timeout, **self._kwargs_dict)
-        return self.do_open(build, request)
 
 
 ##### PEP8 #####
