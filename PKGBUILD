4fd27c60f04ebb82fb3d791337ab30b2e5344a48
Devaev Maxim <mdevaev@gmail.com>
2013-12-25 14:30:19 +0400

    Version bump: 0.21

--- PKGBUILD
+++ PKGBUILD
@@ -1,7 +1,7 @@
 # Contributor: Devaev Maxim <mdevaev@gmail.com>
 
 pkgname=python-ulib-git
-pkgver="0.20"
+pkgver="0.21"
 pkgrel=1
 pkgdesc="Useful python library"
 arch=('any')
--- ulib/const.py
+++ ulib/const.py
@@ -1,5 +1,5 @@
 ##### Public constants #####
-VERSION_TUPLE = (0, 20)
+VERSION_TUPLE = (0, 21)
 VERSION = ".".join(map(str, VERSION_TUPLE))
 
 UPSTREAM_URL = "https://github.com/mdevaev/ulib"
